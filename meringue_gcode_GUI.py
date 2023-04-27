2702bbe11184aa8b5acf56bc443e2f7437203d57
grondl <51844584+grondl@users.noreply.github.com>
2023-04-27 08:37:13 -0400

    Add files via upload

---
+++ meringue_gcode_GUI.py
@@ -0,0 +1,279 @@
+#!/usr/bin/env python3
+
+import tkinter as tk
+from tkinter import messagebox as msg
+from tkinter.ttk import Notebook
+from tkinter import filedialog as fidi
+import re   # Regular Expressions
+import os
+import subprocess
+
+import requests
+
+class MeringueTool(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.nparas = [ "nX","nY","oX","oY","sX","sY","sV","re","mH","mD","nS" ]
+        self.tparas = [ "ho","sh" ]
+
+        self.title("Meringue G-Code generator v1")
+        self.geometry("850x650")
+
+        self.notebook = Notebook(self)
+
+        root_tab = tk.Frame(self.notebook)
+        gcode_tab = tk.Frame(self.notebook)
+
+        # root tab/page setup
+        self.notebook.add(root_tab, text="Parameters/execution")
+
+
+
+        self.label_noX = tk.Label(root_tab, text = "number of buds across (X)  : ", fg = 'black', bg = 'snow')
+        self.label_noY = tk.Label(root_tab, text = "number of buds along  (Y)  : ", fg = 'black', bg = 'snow')
+        self.label_ofX = tk.Label(root_tab, text = "X offset  : ", fg = 'black', bg = 'snow')
+        self.label_ofY = tk.Label(root_tab, text = "Y offset  : ", fg = 'black', bg = 'snow')
+        self.label_spX = tk.Label(root_tab, text = "X spacing  : ", fg = 'black', bg = 'snow')
+        self.label_spY = tk.Label(root_tab, text = "Y spacing  : ", fg = 'black', bg = 'snow')
+        self.label_tot = tk.Label(root_tab, text = "total number of buds  : ",  fg = 'black', bg = 'azure')
+        self.label_fiN = tk.Label(root_tab, text = "filename : ",  fg = 'black', bg = 'azure')
+        self.label_sqV = tk.Label(root_tab, text = "plunger travel (mm) => Squirt volume)  : ", fg = 'black', bg = 'snow')
+        self.label_ret = tk.Label(root_tab, text = "retraction (mm)  : ", fg = 'black', bg = 'snow')
+        self.label_meH = tk.Label(root_tab, text = "meringe height (mm)  : ", fg = 'black', bg = 'snow')
+        self.label_meD = tk.Label(root_tab, text = "meringue diameter (mm)  : ", fg = 'black', bg = 'snow')
+        self.label_noS = tk.Label(root_tab, text = "number of swirls  : ", fg = 'black', bg = 'snow')
+        self.label_hom = tk.Label(root_tab, text = "homing after (yes/no)  : ", fg = 'black', bg = 'snow')
+        self.label_sha = tk.Label(root_tab, text = "shape (bud/kiss/cone)  : ", fg = 'black', bg = 'snow')
+
+        self.label_noX.grid(row =  1, column = 0, padx = 5, pady = 5 , sticky='e')
+        self.label_noY.grid(row =  2, column = 0, padx = 5, pady = 5 , sticky='e')
+        self.label_ofX.grid(row =  3, column = 0, padx = 5, pady = 5 , sticky='e')
+        self.label_ofY.grid(row =  4, column = 0, padx = 5, pady = 5 , sticky='e')
+        self.label_spX.grid(row =  5, column = 0, padx = 5, pady = 5 , sticky='e')
+        self.label_spY.grid(row =  6, column = 0, padx = 5, pady = 5 , sticky='e')
+        self.label_tot.grid(row =  7, column = 0, padx = 5, pady = 5 , sticky='e')
+        self.label_fiN.grid(row =  8, column = 0, padx = 5, pady = 5,  sticky='e')
+        self.label_sqV.grid(row =  9, column = 0, padx = 5, pady = 5,  sticky='e')
+        self.label_ret.grid(row = 10, column = 0, padx = 5, pady = 5,  sticky='e')
+        self.label_meH.grid(row = 11, column = 0, padx = 5, pady = 5,  sticky='e')
+        self.label_meD.grid(row = 12, column = 0, padx = 5, pady = 5,  sticky='e')
+        self.label_noS.grid(row = 13, column = 0, padx = 5, pady = 5,  sticky='e')
+        self.label_hom.grid(row = 14, column = 0, padx = 5, pady = 5,  sticky='e')
+        self.label_sha.grid(row = 15, column = 0, padx = 5, pady = 5,  sticky='e')
+
+
+        self.nX = tk.Entry(root_tab,validate="key", validatecommand=(self.register(self.onValidateInt  ), '%P'), width=5)
+        self.nY = tk.Entry(root_tab,validate="key", validatecommand=(self.register(self.onValidateInt  ), '%P'), width=5)
+        self.oX = tk.Entry(root_tab,validate="key", validatecommand=(self.register(self.onValidateInt), '%P'), width=8)
+        self.oY = tk.Entry(root_tab,validate="key", validatecommand=(self.register(self.onValidateInt), '%P'), width=8)
+        self.sX = tk.Entry(root_tab,validate="key", validatecommand=(self.register(self.onValidateFloat), '%P'), width=8)
+        self.sY = tk.Entry(root_tab,validate="key", validatecommand=(self.register(self.onValidateFloat), '%P'), width=8)
+        self.to = tk.Entry(root_tab, width=6)
+        self.fN = tk.Entry(root_tab, width=40)
+        self.sV = tk.Entry(root_tab,validate="key", validatecommand=(self.register(self.onValidateFloat), '%P'), width=8)
+        self.re = tk.Entry(root_tab,validate="key", validatecommand=(self.register(self.onValidateFloat), '%P'), width=8)
+        self.mH = tk.Entry(root_tab,validate="key", validatecommand=(self.register(self.onValidateFloat), '%P'), width=6)
+        self.mD = tk.Entry(root_tab,validate="key", validatecommand=(self.register(self.onValidateFloat), '%P'), width=6)
+        self.nS = tk.Entry(root_tab,validate="key", validatecommand=(self.register(self.onValidateFloat), '%P'), width=5)
+        self.ho = tk.Entry(root_tab, width=10)
+        self.sh = tk.Entry(root_tab, width=10)
+        self.of = tk.Entry(root_tab, width=60)
+
+
+        self.nX.grid(row =  1, column = 1, padx = 5, pady = 5, sticky='w')
+        self.nY.grid(row =  2, column = 1, padx = 5, pady = 5, sticky='w')
+        self.oX.grid(row =  3, column = 1, padx = 5, pady = 5, sticky='w')
+        self.oY.grid(row =  4, column = 1, padx = 5, pady = 5, sticky='w')
+        self.sX.grid(row =  5, column = 1, padx = 5, pady = 5, sticky='w')
+        self.sY.grid(row =  6, column = 1, padx = 5, pady = 5, sticky='w')
+        self.to.grid(row =  7, column = 1, padx = 5, pady = 5, sticky='w')
+        self.fN.grid(row =  8, column = 1, padx = 5, pady = 5, sticky='w', columnspan = 2)
+        self.sV.grid(row =  9, column = 1, padx = 5, pady = 5, sticky='w')
+        self.re.grid(row = 10, column = 1, padx = 5, pady = 5, sticky='w')
+        self.mH.grid(row = 11, column = 1, padx = 5, pady = 5, sticky='w')
+        self.mD.grid(row = 12, column = 1, padx = 5, pady = 5, sticky='w')
+        self.nS.grid(row = 13, column = 1, padx = 5, pady = 5, sticky='w')
+        self.ho.grid(row = 14, column = 1, padx = 5, pady = 5, sticky='w')
+        self.sh.grid(row = 15, column = 1, padx = 5, pady = 5, sticky='w')
+        self.of.grid(row = 16, column = 1, padx = 5, pady = 5, sticky='w', columnspan = 3)
+
+
+        # Create  open and save file, submit and reset buttons - and attache them
+        self.button_open =   tk.Button(root_tab, text = "open", bg = "green2", fg = "black", command = self.file_open)
+        self.button_save =   tk.Button(root_tab, text = "save", bg = "orange", fg = "black", command = self.file_save)
+        self.button_submit = tk.Button(root_tab, text = "Calculate Total",   bg = "green2", fg = "black", command = self.calculate)
+        self.button_reset =  tk.Button(root_tab, text = "Reset all values",    bg = "red",    fg = "black", command = self.default_all)
+        self.open_button =   tk.Button( root_tab, text='Open a File', command=self.file_open )
+        self.proceed_button = tk.Button(root_tab, text="Produce G-Code", command=self.process)
+        self.test_button = tk.Button(root_tab, text="run test", command=self.runtest)
+
+
+       # submit, reset, open/save  file buttons
+        self.button_submit.grid(  row = 1,  column = 3, pady = 5)
+        self.button_reset.grid(   row = 2,  column = 3, pady = 5)
+        self.open_button.grid(    row = 8,  column=  3, padx = 5, pady = 5)
+        #self.button_open.grid(   row = 7,  column = 3, pady = 5)
+        self.button_save.grid(    row = 8,  column = 4, pady = 5)
+        self.proceed_button.grid( row = 18, column = 0, padx = 5, pady = 5)
+        self.test_button.grid( row = 18, column = 3, padx = 5, pady = 5 )
+
+        # GCode tab/page setup
+        self.notebook.add(gcode_tab, text="G-Code")
+
+        self.param_gcgen = tk.Text(gcode_tab, bg="white", fg="black", width = 780, height=32)
+        self.param_gcgen.pack(side=tk.TOP, expand=1)
+
+        self.gcode_copy_button = tk.Button(gcode_tab, text="Copy to Clipboard", command=self.copy_to_clipboard)
+        self.gcode_copy_button.pack(side=tk.BOTTOM, fill=tk.X)
+
+        self.gcode_status = tk.StringVar(gcode_tab)
+        self.gcode_status.set("--")
+
+        self.gcode_label = tk.Label(gcode_tab, textvar=self.gcode_status, bg="lightgrey", fg="black")
+        self.gcode_label.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        self.notebook.pack(fill=tk.BOTH, expand=1)
+        self.default_all()
+
+    def onValidateFloat(self, P ):
+        pattern = r"[-+]?\d*\.?\d*"
+        if re.fullmatch(pattern, P) is None:
+            self.bell()
+            return False
+        else:
+            return True
+
+    def onValidateInt(self, P ):
+        pattern = r"[-+]?\d*"
+        if re.fullmatch(pattern, P) is None:
+            self.bell()
+            return False
+        else:
+            return True
+
+    def process(self,text=None):
+        if self.verify_parameters() == False:
+            return
+        if not text:
+            text = " ".join(["python", "meringues_gcode_generator_para.py" , "-x" , self.nX.get(), "-y", self.nY.get(), "-a" , self.oX.get(), "-b", self.oY.get(),
+                                                                             "-i" , self.sX.get(), "-j", self.sY.get(), "-p" , self.sh.get(), "-m" , self.ho.get() ])
+        try:
+            self.gcode_status.set(text)
+            gcode_txt = subprocess.run(["python", "meringues_gcode_generator_para.py" , "-x" , self.nX.get(), "-y" , self.nY.get(),
+                                                                                       "-a" , self.oX.get(), "-b" , self.oY.get(),
+                                                                                        "-i" , self.sX.get(), "-j" , self.sY.get(),
+                                                                                        "-p" , self.sh.get(), "-m" , self.ho.get(),
+                                                                                          ]  , capture_output=True, text=True, check=True, timeout= 3)
+
+
+            self.param_gcgen.delete(1.0,tk.END)
+            self.param_gcgen.insert(1.0,gcode_txt.stdout)
+            msg.showinfo("Gcode produced", "meringues_gcode_generator_param.py executed\n see G-Code tab for result")
+        except Exception as e:
+            msg.showerror("G-Code production failed" + text ,  str(e))
+
+    def copy_to_clipboard(self, text=None):
+        if not text:
+            text = self.param_gcgen.get(1.0,tk.END)
+        self.clipboard_clear()
+        self.clipboard_append(text)
+        msg.showinfo("Copied Successfully", "Text copied to clipboard")
+
+    def file_open(self):
+        # file type
+        filetypes = (('text files', '*.gco'), ('All files', '*.*') )
+        # show the open file dialog
+        f = fidi.askopenfile(filetypes=filetypes)
+        fn = f.name
+        dn = os.path.dirname(fn)
+        bn = os.path.basename(fn)
+        self.of.delete(0,tk.END)
+        self.of.insert(0,dn)
+        self.fN.delete(0,tk.END)
+        self.fN.insert(0,bn)
+        data = f.read()
+        self.param_gcgen.delete(1.0, tk.END)
+        self.param_gcgen.insert(1.0, data)
+        #print(data)
+        f.close()
+
+    def file_save(self, text=None):
+        fin = self.fN.get()
+        if ( fin is not None) and ( fin !='') and ( fin != "n.d." ):
+            with open(fin, 'w') as f:
+                content = self.param_gcgen.get('1.0', tk.END)
+                f.write(content)
+                msg.showinfo("File written", fin + " ... saved to local directory")
+                f.close
+        else:
+            msg.showinfo("No file written", "file name not defined or empty")
+
+    def runtest(self):
+        param = " "
+        for par in self.nparas:
+            parv = eval("self." + par + ".get()" )
+            if ( parv is not None) and ( parv !='') :
+               param += par + "= " + eval("self." + par + ".get()" ) + " "
+        msg.showinfo( " result ", param )
+
+    def verify_parameters(self):
+        for par in self.nparas:
+            parv = eval("self." + par + ".get()" )
+            if ( parv is None) or ( parv == '') :
+               msg.showinfo( "one or more missing parameter ",par )
+               return False
+        if (self.ho.get()) is None or ( self.ho.get() == '' ) :
+            msg.showinfo( "Problem","one or more missing parameter (--home)" )
+            return False
+        if (self.sh.get()) is None or ( self.sh.get() == '' ) :
+            msg.showinfo( "Problem ","one or more missing parameter (--shape)" )
+            return False
+        return True
+
+
+
+    def calculate(self, text=None):
+        # get a content from entry box
+        vnX = int(self.nX.get())
+        vnY = int(self.nY.get())
+        voX = int(self.oX.get())
+        voY = int(self.oY.get())
+
+        # Calculates compound interest
+        if (vnY != '' ) and (vnY != '') :
+            number = vnX * vnY
+        else:
+            number = 0
+
+        # insert method inserting the value in the text entry box.
+        self.to.delete(0, tk.END)
+        self.to.insert(1, number)
+
+    def default_all(self, text=None):
+        # whole content of entry boxes set to default values
+
+        self.nX.delete(0, tk.END) ; self.nX.insert(0,"2")
+        self.nY.delete(0, tk.END) ; self.nY.insert(0,"2")
+        self.oX.delete(0, tk.END) ; self.oX.insert(0,"20")
+        self.oY.delete(0, tk.END) ; self.oY.insert(0,"20")
+        self.sX.delete(0, tk.END) ; self.sX.insert(0,"25")
+        self.sY.delete(0, tk.END) ; self.sY.insert(0,"25")
+        self.to.delete(0, tk.END) ; self.to.insert(0,"4")
+        self.fN.delete(0, tk.END) ; self.fN.insert(0,"n.d.")
+        self.sV.delete(0, tk.END) ; self.sV.insert(0,"1.8")
+        self.re.delete(0, tk.END) ; self.re.insert(0,"9.0")
+        self.mH.delete(0, tk.END) ; self.mH.insert(0,"12.0")
+        self.mD.delete(0, tk.END) ; self.mD.insert(0,"13.0")
+        self.nS.delete(0, tk.END) ; self.nS.insert(0,"3.0")
+        self.ho.delete(0, tk.END) ; self.ho.insert(0,"no")
+        self.sh.delete(0, tk.END) ; self.sh.insert(0,"kiss")
+        self.of.delete(0, tk.END) ; self.of.insert(0,"n.d.")
+
+        # set focus on  an entry box
+        self.nX.focus_set()
+
+
+if __name__ == "__main__":
+    meringuetool = MeringueTool()
+    meringuetool.mainloop()
---
+++ meringues_gcode_generator_para.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import math
+
+
+# default values
+sqvol = 2.2
+totsqvol = 0
+retract =1.8
+
+xrows = 6
+ycolumns = 7
+xspacing = 35
+xoffset = 20
+yspacing = 36
+yoffset = 3
+mer_diameter =  3
+mer_height = 10
+swirls = 3
+homing = False
+eshape = 'kiss'
+list_of_shapes = [ 'kiss', 'coin' , 'code ' ]
+
+
+
+def gcode_header():
+  print("; Header start")
+  print("M302 S0 ; Allow cold extrusion") # allow cold extrusion
+  print("G92 E0 ; Zero the extrusion counter")  # ZERO extrusion counter
+  print("M18 S3600 ; lock motors for one hour when idle")
+  print("G0 Z50 ; Raise Z off its limit switch")
+  print("; Header end")
+
+def gcode_footer(homing=False):
+  print("; Footer start" )
+  print("G0 Z80")
+  if homing:
+      print("G0 X20 Y20 ; homing=yes ... extrusion head moved close to home ")
+  else:
+      print("; stay there , do not drip on way back or sweep accross top of kisses  .. removed G0 X20 Y20")
+  print("; Footer end")
+
+def gcode_coin( tsqvol , sqvol , xpos, ypos, mer_diameter):
+  print("; Bud is coin shaped ")
+  print( "G1 X{0} Y{1} F800".format( xpos ,ypos) )
+  print( "G1 Z2 F600")
+  # print("G92 E0")
+  tsqvol+= sqvol
+  merr  = mer_diameter / 2
+  print( "G2 I{0} J{1} F100 E{2}".format( merr,merr, tsqvol))
+  tsqvol +=1
+  print( "G1 X{0} Y{1} Z{2} F100 E{3}".format(xpos+merr , ypos+merr , mer_diameter ,  tsqvol ))
+  print( "G4 P5000")
+  print("G1 Z50 F600")
+  # print ... G1 E0 F600
+  print("; Bud end")
+  return( tsqvol );
+
+def gcode_kiss( tsqvol , sqvol ,retract, xpos, ypos, mer_diameter):
+  print("; Bud is kiss shaped")
+  print( "G1 X{0} Y{1} F2800".format( xpos ,ypos) )
+  print( "G1 Z0 F1500")
+  # print("G92 E0")
+  tsqvol+= sqvol
+  merr  = mer_diameter / 2
+  print( "G1 Z8 F80 E{0:.3f}".format( tsqvol))
+  tsqvol +=.1
+  print( "G1 X{0} Y{1} Z{2} F100 E{3:.3f}".format(xpos , ypos , mer_height ,  tsqvol ))
+  print( "G1 X{0} Y{1} Z{2} F100 E{3:.3f}".format(xpos , ypos , mer_height+10 ,  tsqvol-retract ))
+  print( "G4 P2000")
+  print( "G1 Z40 F1200")
+  # print ... G1 E0 F600
+  print("; kiss shaped end")
+  return( tsqvol );
+
+def gcode_cone( tsqvol , sqvol , xpos, ypos, mer_diameter, mer_height, swirls):
+  steps = 20
+  print("; bud is Cone start")
+  print( "G1 X{0} Y{1} F800".format( xpos ,ypos) )
+  print( "G1 Z2 F600")
+  zp = 2 ; xpo = xpos ; ypo = ypos ;   zpo = zp
+  # print("G92 E0")
+  merr  = mer_diameter / 2
+  angmax = int(swirls * 360)
+  angst = int( 360 / steps)
+  tle = 0
+  for ang in range(0, angmax  , angst):  # first pass to determine total swirl length
+    rr =   merr * (angmax - ang) / angmax # radius diminishes as swirl is built
+    xp =  xpos + (math.cos( math.radians(ang)) * rr)
+    yp =  ypos + (math.sin( math.radians(ang)) * rr)
+    zp +=  mer_height /( swirls + steps)
+    le = math.sqrt( (xp-xpo) ** 2 + (yp - ypo) ** 2 + (zp - zpo ) ** 2 )
+    xpo = xp
+    ypo = yp
+    zpo = zp
+    tle += le
+  print( "; total swirl squirt length : {0:.3f}mm,{1:.2f} *ml {2:.2f} *ml-tot    for a {3} swirl {4} mm diameter {5} mm high cone ".format(float(tle),float(sqvol), float(tsqvol), swirls, mer_diameter, mer_height) )
+  zp = 2 ; xpo = xpos ; ypo = ypos ;   zpo = zp
+  for ang in range(0, angmax  , angst):
+    rr =   merr * (angmax - ang) / angmax
+    xp =  xpos + (math.cos( math.radians(ang)) * rr)
+    yp =  ypos + (math.sin( math.radians(ang)) * rr)
+    zp +=  mer_height /( swirls + steps)
+    le = math.sqrt( (xp-xpo) ** 2 + (yp - ypo) ** 2 + (zp - zpo ) ** 2 )
+    xpo = xp
+    ypo = yp
+    zpo = zp
+    tsqvol += sqvol * le / tle
+    print( "G1 X{0:.2f} Y{1:.2f} Z{2:.2f} F100 E{3:.2f}".format( xp,yp,zp, tsqvol))
+
+  print( "G1 Z{0:.3f} F100".format(zp + mer_height /2 ))
+  print( "G4 P4000")
+  print("G1 Z50 F600")
+  # print ... G1 E0 F600
+  print("; Cone end")
+  return( tsqvol );
+
+
+
+
+# Main program starts here
+
+
+argParser = argparse.ArgumentParser()
+
+argParser.add_argument("-f", "--ofile", help="output gcode file name ")
+argParser.add_argument("-s", "--sqvol", type=float, help="syringe squirt volume in mm plunger travel")
+argParser.add_argument("-r", "--retract", type=float, help="syringe retraction after squirt in mm plunger travel")
+argParser.add_argument("-x", "--xrows", type=int, help="number of rows X")
+argParser.add_argument("-y", "--ycolumns", type=int, help="number of columns Y")
+argParser.add_argument("-a", "--xoffset", type=int, help="X offset in mm")
+argParser.add_argument("-b", "--yoffset", type=int, help="Y offset ei mm")
+argParser.add_argument("-i", "--xspacing", type=float, help="spacing between buds on X in mm")
+argParser.add_argument("-j", "--yspacing", type=float, help="spacing between buds on Y in mm")
+argParser.add_argument("-w", "--swirls", type=float, help="number of swirls when making a cone")
+argParser.add_argument("-d", "--mer_diameter", type=float, help="meringue diameter in mm")
+argParser.add_argument("-t", "--mer_height", type=float, help="meringue height in mm")
+argParser.add_argument("-m", "--homing", help="home when finished (yes/no)")
+argParser.add_argument("-p", "--shape", help="shape of extrusion (kiss/coin/cone)")
+
+args = argParser.parse_args()
+#print("args=%s" % args)
+#print("args.ofile=%s" % args.ofile)
+
+if args.ofile is not None:
+    ofile = args.ofile
+if args.sqvol is not None:
+    sqvol = args.sqvol
+if args.retract is not None:
+    retract = args.retract
+if args.xrows is not None:
+    xrows = args.xrows
+if args.ycolumns is not None:
+    ycolumns = args.ycolumns
+if args.xoffset is not None:
+    xoffset = args.xoffset
+if args.yoffset is not None:
+    yoffset = args.yoffset
+if args.xspacing is not None:
+    xspacing = args.xspacing
+if args.yspacing is not None:
+    yspacing = args.yspacing
+if args.swirls is not None:
+    swirls = args.swirls
+if args.mer_diameter is not None:
+    mer_diameter = args.mer_diameter
+if args.mer_height is not None:
+    mer_height = args.mer_height
+if args.homing is not None:
+    if args.homing == 'yes':
+        homing = True
+    elif args.homing =='no':
+        homing = False
+    else:
+        homing = False
+if args.shape is not None:
+    if args.shape in list_of_shapes :
+        eshape = args.shape
+    else:
+        eshape = 'kiss'
+
+
+
+
+print('; Python program - Number of arguments:', len(sys.argv), 'arguments.')
+#print('; Argument List:', str(sys.argv))
+print("; sqvol = {0:.2f}mm, retract = {1:.2f}mm, xrows = {2}, ycolumns = {3}, xspacing = {4}, xoffset = {5}, yspacing = {6}, yoffset = {7}, mer_diameter = {8}, mer_height = {9}, swirls = {10} homing = {11} ".format (
+  sqvol, retract, xrows, ycolumns, xspacing, xoffset, yspacing, yoffset, mer_diameter, mer_height, swirls, homing  ))
+
+
+gcode_header()
+
+for j in range( ycolumns): # Y
+  xr = range(xrows)
+  if (j%2) == 1:
+    xr = range(xrows-1,-1,-1)
+  for i in xr: # X
+    mx = xoffset + i * xspacing
+    my = yoffset + j * yspacing
+
+
+    if eshape == "kiss":
+        totsqvol = gcode_kiss( totsqvol , sqvol , retract, mx , my , mer_diameter )
+    elif eshape == "cone":
+        totsqvol = gcode_cone(totsqvol, sqvol, mx, my, mer_diameter, mer_height, swirls)
+    elif eshape == "coin":
+        totsqvol = gcode_coin( totsqvol , sqvol , mx , my , mer_diameter )
+    else:
+        print("pas de forme choisie")
+
+
+gcode_footer(homing)
